287d72681e354aa8cd5922d99374cb8f95a98354
rjs <premonday@gmail.com>
2021-07-22 12:12:09 +0100

    added how to mint conetnt

--- src/pages/HowToMint.js
+++ src/pages/HowToMint.js
@@ -3,6 +3,7 @@
 import UIFooter from '../components/layout/Footer';
 import UIHeader from '../components/layout/Header';
 import { CubeIcon } from "@heroicons/react/outline";
+import HpBg from '../assets/img/hp-bg.jpg';
 
 import { Link } from 'react-router-dom';
 import { Button } from '@windmill/react-ui';
@@ -10,6 +11,20 @@ import { Button } from '@windmill/react-ui';
 // Hooks
 import { useActiveWeb3React } from '../hooks';
 import useAuth from '../hooks/useAuth.js';
+
+const steps = [
+  'Connect your account',
+  'Press "Mint"',
+  'Confirm transaction',
+  'Go to Polkaparty.io'
+];
+
+const stepsDetails = [
+  '',
+  'Lorem ipsum dolor sit amet consectetuar adipiscing nonummy elit adipiscing...',
+  '',
+  'Lorem ipsum dolor sit amet consectetuar adipiscing nonummy elit adipiscing...'
+]
 
 export function IndexPage(props) {
   const { login, logout } = useAuth();
@@ -25,14 +40,27 @@ export function IndexPage(props) {
       />
 
       {/* TODO: Create a Layout module to house the content */}
-      <div className='w-full px-5'>
-        <div className='container my-16 md:my-28'>
+      <div className='w-full px-5 relative'>
+      <img src={HpBg} className="w-full top-0 absolute left-0" alt="" />
+        <div className='container py-16 md:py-28 md:pb-16 relative'>
           <div className='max-w-[700px] mx-auto text-center'>
             <h1 className="bg-clip-text bg-gradient-to-b from-primary to-primary-light text-transparent">
               How to mint
             </h1>
             <h2>Minting your NFTs on PolkaPlace for PolkaParty</h2>
           </div>
+        </div>
+
+        <div className="container-sm flex-row space-y-4 mb-20">
+          {steps.map((step, i) => (
+          <div className="p-8 rounded shadow items-center relative flex">
+            <span className="w-[66px] h-[66px] rounded-full bg-gradient-to-b from-primary to-primary-light md:-translate-x-1/2 md:absolute left-0 items-center text-white justify-center flex font-brand font-bold text-lg">{i+1}</span>
+            <div className="ml-5">
+            <h3 className="mb-0">{step}</h3>
+            {stepsDetails[i] ? <p className="mb-0">{stepsDetails[i]}</p> : ''}
+            </div>
+          </div>
+          ))}
         </div>
       </div>
 
